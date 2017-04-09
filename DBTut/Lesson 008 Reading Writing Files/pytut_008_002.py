252539edb72de27e392f6bee9488c6e39c9effa5
s3icc0 <ondrej.sisler@gmail.com>
2017-04-10 00:21:34 +0200

    DBTut continued

--- DBTut/Lesson 008 Reading Writing Files/pytut_008_002.py
+++ DBTut/Lesson 008 Reading Writing Files/pytut_008_002.py
@@ -7,21 +7,21 @@ import os
 with open('mydata2.txt', mode='w', encoding='utf-8') as myFile:
     myFile.write('Some random text\nMore random text\nAnd some more')
 
-with open('mydata2.txt', encoding='utf-8') as myFile:
-
-    # keep track of line number
-    linenum = 1
-
-    # loop through unknown number of lines
-    while True:
-
-        # read one line up to newline
-        line = myFile.readline()
-
-        # if line has no data stop reading
-        if not line:
-            break
-
-        print('Line', linenum, ' : ', line, end='')
-
-        linenum += 1
+# with open('mydata2.txt', encoding='utf-8') as myFile:
+#
+#     # keep track of line number
+#     linenum = 1
+#
+#     # loop through unknown number of lines
+#     while True:
+#
+#         # read one line up to newline
+#         line = myFile.readline()
+#
+#         # if line has no data stop reading
+#         if not line:
+#             break
+#
+#         print('Line', linenum, ' : ', line, end='')
+#
+#         linenum += 1
